8cd9076cd18638b4bdb00b4e25c88d49c1a461ee
N.MEZIANY <mez.blockchain@gmail.com>
2024-08-13 17:58:49 +0300

    added content to the components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 3, packed: false },
+  { id: 5, description: "Shoes", quantity: 2, packed: true },
+  { id: 3, description: "Laptop", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -15,14 +23,43 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
-      <h3>What do you need for your 😍 trip</h3>
-    </div>
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  const itemStyle = { textDecoration: "line-through", color: "gray" };
+  return (
+    <li>
+      <span style={item.packed ? itemStyle : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {

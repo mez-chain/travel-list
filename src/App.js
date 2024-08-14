64e741dec9c3af009ae7818813f929da59838c5b
N.MEZIANY <mez.blockchain@gmail.com>
2024-08-15 02:36:32 +0300

    fixed bugs and added sorting and clear button functions

--- src/App.js
+++ src/App.js
@@ -42,6 +42,11 @@ export default function App() {
     );
   }
 
+  function handleClearItems() {
+    setItems([]);
+    localStorage.setItem("items", JSON.stringify([]));
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -50,6 +55,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onUpdateItem={handleUpdateItem}
+        onClearItems={handleClearItems}
       />
       <Stats items={items} />
     </div>
@@ -99,11 +105,32 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onUpdateItem }) {
+function PackingList({ items, onDeleteItem, onUpdateItem, onClearItems }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  function togglePopup() {
+    setShowPopup(!showPopup);
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -112,6 +139,21 @@ function PackingList({ items, onDeleteItem, onUpdateItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">SORT BY INPUT ORDER</option>
+          <option value="description">SORT BY DESCRIPTION</option>
+          <option value="packed">SORT BY PACKED STATUS</option>
+        </select>
+
+        <button onClick={togglePopup}>CLEAR LIST</button>
+        <ChoicePopup
+          togglePopup={togglePopup}
+          showPopup={showPopup}
+          onClearItems={onClearItems}
+        />
+      </div>
     </div>
   );
 }
@@ -157,5 +199,22 @@ function Stats({ items }) {
         you already packed ${totalPackedItems} (${percentage}%)`}
       </em>
     </footer>
+  );
+}
+
+function ChoicePopup({ showPopup, onClearItems, togglePopup }) {
+  function handleChoice(choice) {
+    if (choice === "yes") onClearItems();
+    else togglePopup();
+    togglePopup();
+  }
+  return (
+    showPopup && (
+      <div className="popup">
+        <h3>Do you want to clear the list?</h3>
+        <button onClick={() => handleChoice("yes")}>Yes</button>
+        <button onClick={() => handleChoice("no")}>No</button>
+      </div>
+    )
   );
 }
